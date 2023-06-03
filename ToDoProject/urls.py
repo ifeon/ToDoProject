4dcc599a1cb1ef61084259f991f804cfe7843588
Alex <alpn2802@gmail.com>
2023-06-03 16:19:12 +0300

    main app, main page

--- ToDoProject/urls.py
+++ ToDoProject/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import main.views
+from main import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.main)
 ]

3797ae64a0eeae0b1c79f8ede5f7f9d36690f2a0
Alex <alpn2802@gmail.com>
2023-09-19 19:39:46 +0300

    new urls - add task, task detail

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,6 @@ app_name = 'main'
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('done/', views.task_done, name='task_done'),
-    path('<int:id>/', views.task_detail, name='task_detail'),
+    path('add/', views.task_create, name='add_task'),
+    path('task/<int:id>/', views.task_detail, name='task_detail'),
 ]

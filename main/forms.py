73a4012a75886f6db742b242e9c1400895aeb9ab
Alex <alpn2802@gmail.com>
2023-09-19 19:38:42 +0300

    new form

--- main/forms.py
+++ main/forms.py
@@ -1,8 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import ModelForm
+
+from main.models import Task
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+
+class TaskCreationForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'detail', 'completed', 'username']
